889aa24b9c264cc69f60319ef9361cf5a2924ab3
kheysialailan <kheysialailanadzriani@gmail.com>
2024-08-22 11:01:50 +0700

    Halaman login

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login Glow in the Dark</title>
+</head>
+<body>
+    <div class="login-box">
+        <h2>Login Keren</h2>
+        <form>
+          <div class="user-box">
+            <input type="text" name="" required="">
+            <label>Username</label>
+          </div>
+          <div class="user-box">
+            <input type="password" name="" required="">
+            <label>Password</label>
+          </div>
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Submit
+          </a>
+           <div class="last-info">
+                    <div class="one">
+                        Remember Me
+                        <input type="checkbox" id="thing">
+                        <label for="thing"></label>
+                    </div>
+                    <div class="two">
+                        <a href="#">Create Account</a>
+                    </div>
+                    <div class="three">
+                        <a href="#">Forget Password ?</a>
+                    </div>
+                </div>
+        </form>
+      </div>
+</body>
+</html>
